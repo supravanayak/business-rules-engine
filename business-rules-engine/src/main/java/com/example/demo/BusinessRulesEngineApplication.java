0d21cf021dcd5922d8f3bd1fc7a0182fea4079c3
supravanayak <suprava.nayak013@gmail.com>
2020-09-16 00:45:11 +0530

    Initial Structure commit

---
+++ business-rules-engine/src/main/java/com/example/demo/BusinessRulesEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessRulesEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessRulesEngineApplication.class, args);
+	}
+
+}
