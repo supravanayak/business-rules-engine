669e493893b6dfd01ef7d0915e968aec80492769
supravanayak <suprava.nayak013@gmail.com>
2020-09-16 01:01:49 +0530

    First commit

---
+++ business-rule-engine/src/main/java/fr/maersk/interview/BusinessRuleEngineApplication.java
@@ -0,0 +1,13 @@
+package fr.maersk.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessRuleEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessRuleEngineApplication.class, args);
+	}
+
+}
---
+++ business-rule-engine/src/test/java/fr/maersk/interview/BusinessRuleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.maersk.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessRuleEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
